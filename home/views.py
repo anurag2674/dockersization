21c7480d2118fa7f7a06b37dda389fb4448434dd
anurag2674 <g.anuragbobby@gmail.com>
2022-08-07 23:39:31 +0530

    First commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Contact
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content =request.POST['content']
+        contact=Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
+    return render(request, "contact.html")
